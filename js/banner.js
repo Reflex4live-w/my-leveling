bb704ede3f30cb0eb7b49649d481848ef6478855
Pakhomkin Vlad <vladrd4@gmail.com>
2026-09-05 12:42:47 +0300

    banner-time-date-avatar

--- js/banner.js
+++ js/banner.js
@@ -8,12 +8,34 @@ function timedate() {
   };
 
   const optionsDate = {
-    weekday: 'short',
+    // weekday: 'short',
     year: 'numeric',
     month: '2-digit',
-    day: 'numeric',
+    day: '2-digit',
   };
 
   document.getElementById('time').textContent = now.toLocaleString('ru-RU', optionsTime);
   document.getElementById('date').textContent = now.toLocaleString('ru-RU', optionsDate);
 };
+
+timedate();
+setInterval(timedate, 1000);
+
+
+
+//dota 2 rang
+const dotaInput = document.getElementById('dota-input');
+
+document.addEventListener('DOMContentLoaded', function() {
+  const saved = localStorage.getItem('myInputValue');
+  if (saved !== null) {
+    dotaInput.value = saved;
+  }
+});
+
+dotaInput.addEventListener('input', function() {
+  localStorage.setItem('myInputValue', this.value);
+});
+
+
+
